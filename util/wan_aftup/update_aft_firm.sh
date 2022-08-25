c193e9825d36a157f08005c61eea1bba2bb04c14
Sangoma <>
2022-08-25 00:25:50 -0400

    wanpipe-7.0.34.2.tgz

--- util/wan_aftup/update_aft_firm.sh
+++ util/wan_aftup/update_aft_firm.sh
@@ -34,12 +34,13 @@ eval "./scripts/unload.sh"
 if [ -f "/tmp/fw-upd-err.txt" ]; then
 	eval 'grep -nr "Failed with" /tmp/fw-upd-err.txt  | grep "manufcaturer ID as FF" >/dev/null 2>/dev/null'
 	if [ $? -eq 0 ]; then
-		 echo ""
-		 echo "****************************************************************************************"
-		 echo -e " Please \e[31m\e[4mREBOOT SYSTEM\e[0m and Try Firmware Upgrade Again to ensure the card functionality!! "
-		 echo "****************************************************************************************"
+		echo ""
+		echo "*************************************************************************************************"
+		echo -e "            The firmware update may have \e[1;30mchanged the PCIe Address of the card\e[0m."
+		echo -e "  Please \e[1;31m\e[4mREBOOT SYSTEM\e[0m and Re-Run the procedure to ensure firmware upgrade has been successful."
+		echo "*************************************************************************************************"
 
-		 rm -rf /tmp/fw-upd-err.txt
+		rm -rf /tmp/fw-upd-err.txt
 	fi
 fi
 
